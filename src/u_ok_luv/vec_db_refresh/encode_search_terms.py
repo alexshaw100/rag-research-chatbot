e5145dabc181a0baf4f9437bd8c681f9e943bfad
Alex <alex.shaw@deeprender.ai>
2025-09-04 22:32:28 +0100

    encoding sensitive search terms

---
+++ src/u_ok_luv/vec_db_refresh/encode_search_terms.py
@@ -0,0 +1,34 @@
+"""
+For now all search terms will be encoded in Github using base64 encoding.
+
+Before searching using the Arxiv command line tool, these search terms will
+be decoded.
+"""
+import argparse
+import base64
+import pandas as pd
+
+def encode_search_terms(csv: str):
+    """
+    Encodes the items in a single column csv using base64.
+    """
+    c = pd.read_csv(csv)
+    c['Term'] = c['Term'].map(lambda t: base64.b64encode(t.encode('utf8')).decode('utf8'))
+    c.to_csv(csv, index=False)
+    return c['Term']
+
+def decode_search_terms(csv: str):
+    """
+    Encodes the items in a single column csv using base64.
+    """
+    c = pd.read_csv(csv)
+    c['Term'] = c['Term'].map(lambda t: base64.b64decode(t.encode('utf8')).decode('utf8'))
+    c.to_csv(csv, index=False)
+    return c['Term']
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', help='The path to the csv file with the terms to be encoded or decoded.')
+    args = parser.parse_args()
+
+    print(encode_search_terms(args.path))
